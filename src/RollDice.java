a6dc9b05862b7af295184fd9dc0559c974f8fbbe
Zalika Ockleberry <Ockleberryzalika@gmail.com>
2016-09-14 11:44:04 -0400

    Still broken

--- src/RollDice.java
+++ src/RollDice.java
@@ -6,20 +6,27 @@ public class RollDice {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to the Grand Circus Casino! Roll the dice? (y/n): ");
 		
 		System.out.println();
 		//Create Scanner
 		Scanner input = new Scanner(System.in);
-			String  choice;
+		String  choice = "y";
+			
+		
+		//While loop
+		
+		while (choice.equalsIgnoreCase("y")){
+		
 			
 		//User prompt
+		System.out.println("Welcome to the Grand Circus Casino! Roll the dice? (y/n): ");
+		input.nextLine();
 		System.out.println("Roll 1:");
 		
 		//Create Random 
 		Random diceRoll = new Random(0);
-		int roll1 = 1 + diceRoll.nextInt(6);
-		int roll2 = 2 + diceRoll.nextInt(6);
+		int roll1 = diceRoll.nextInt(100);
+		int roll2 = diceRoll.nextInt(100);
 		
 		//for loop
 		for (int count = 1; count <= 10; count++);
@@ -27,8 +34,14 @@ public class RollDice {
 		System.out.println(roll2);
 		
 		//Continue prompt
-		System.out.println("Roll again?(y/n");
+		System.out.println("Roll again?(y/n)");
+		input.nextLine();
+		choice = input.next();
 		
+		//Thank you message
+		System.out.println("Thanks for gambling!");
+		
+		}
 	}
 
 
